d86d7024eda7cdb64bd114d452c197d7ccf12c35
CoderYihaoWang <yesiamyihao@gmail.com>
2020-07-14 23:19:34 +1200

    Add SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { IUserInput } from './Common/interfaces';
+import SearchBar from './Components/SearchBar/SearchBar';
 
 function App() {
+  const [UserInput, setUserInput] = useState<IUserInput>({
+    SearchQuery: "Mars",
+    StartDate: new Date('2014-08-18T21:11:54'),
+    EndDate: new Date('2018-08-18T21:11:54'),
+  });
+
+  function SetUserInput(a: IUserInput) {
+    setUserInput(a);    
+  }
   return (
     <div>
+      <SearchBar SetUserInput={(a: IUserInput) => SetUserInput(a)}></SearchBar>
     </div>
   );
 }
