d8d2936c3a4f9e4b9d6ee26427339b954dfeef10
CoderYihaoWang <yesiamyihao@gmail.com>
2020-07-14 23:44:41 +1200

    Add other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 import './App.css';
-import { IUserInput } from './Common/interfaces';
 import SearchBar from './Components/SearchBar/SearchBar';
+import MediaGrid from './Components/MediaGrid/MediaGrid';
+import { IUserInput } from './Common/interfaces';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 700,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+})
 
 function App() {
   const [UserInput, setUserInput] = useState<IUserInput>({
@@ -11,8 +25,11 @@ function App() {
   });
 
   return (
-    <div>
-      <SearchBar SetUserInput={setUserInput}></SearchBar>
+    <div className="App" >
+      <MuiThemeProvider theme={theme}>
+          <SearchBar SetUserInput={setUserInput}/>
+          <MediaGrid SearchQuery={UserInput.SearchQuery} StartDate={UserInput.StartDate} EndDate={UserInput.EndDate}/>
+      </MuiThemeProvider>
     </div>
   );
 }
